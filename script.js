6109027a1cd6f0fbee6b3450374b2ed276a4a978
Rustam <rustam2004sadulaev@mail.ru>
2022-02-08 11:15:55 +0300

    task-26

--- script.js
+++ script.js
@@ -165,6 +165,7 @@ const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
 
 
 //----------------------------------------------------26
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 
 
 //----------------------------------------------------27
