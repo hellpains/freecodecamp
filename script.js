cf06b3990d6e7f346ee00619c2f62be8631b0e03
Rustam <rustam2004sadulaev@mail.ru>
2022-02-08 11:27:42 +0300

    task-29

--- script.js
+++ script.js
@@ -177,6 +177,8 @@ const myStr = "This is the start. " + "This is the end.";
 
 
 //----------------------------------------------------29
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence."
 
 
 //----------------------------------------------------30
